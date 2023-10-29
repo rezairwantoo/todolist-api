d9105bd7205843b0c1a956c66bcdb7d3d48c8b30
reza.irwanto <reza.irwantoo@gmail.com>
2023-10-29 23:27:18 +0700

    chore: update ddl and postman collection

--- resources/postgres/ddl/ddl.sql
+++ resources/postgres/ddl/ddl.sql
@@ -1,9 +1,17 @@
-create table products (
-	id serial CONSTRAINT rmr_remisier_pk PRIMARY KEY,
-	item VARCHAR(255) null,
-	price bigint null,
-	is_active bool null,
-	deleted_at timestamp null,
-	created_at timestamp default CURRENT_TIMESTAMP null,
-	updated_at timestamp default CURRENT_TIMESTAMP null
+-- public.tasks definition
+
+-- Drop table
+
+-- DROP TABLE public.tasks;
+
+CREATE TABLE public.tasks (
+	id serial4 NOT NULL,
+	todo_id int8 NULL,
+	title varchar(255) NOT NULL,
+	description text NOT NULL,
+	file text NULL,
+	deleted_at timestamp NULL,
+	created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT todo_pk PRIMARY KEY (id)
 );
